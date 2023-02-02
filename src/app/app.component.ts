bdf297c42cdeb72bb6d784b487bd2c970a6efae4
jonifz <fernandezjonatan.m@gmail.com>
2023-02-02 07:35:15 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class MyService{
+  constructor(){}
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  /*nombre:string='';
+  telefono='';
+  dni='';
+
+
+ cambiaDni(valor) {
+ console.log(valor);
+ this.dni=valor;
+ }
+
+onClick() {
+  alert('Hiciste click!');
+  */
+}
